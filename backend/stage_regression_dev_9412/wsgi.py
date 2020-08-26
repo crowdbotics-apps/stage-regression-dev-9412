e08dc2202caae1b3762216616b8ae8e5ef32519e
Crowdbotics <team@crowdbotics.com>
2020-08-26 17:43:48 +0000

    Update models

--- backend/stage_regression_dev_9412/wsgi.py
+++ backend/stage_regression_dev_9412/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stage_regression_dev_9412.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "stage_regression_dev_9412.settings")
 
 application = get_wsgi_application()
